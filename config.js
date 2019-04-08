60c6fd6a0e5babba6909541f00c750aedeffb06e
jaclynmyers <49379164+jaclynmyers@users.noreply.github.com>
2019-04-07 20:31:31 -0400

    Update config.js

--- config.js
+++ config.js
@@ -2,16 +2,16 @@ module.exports = {
   // SPREADSHEET DETAILS, this is where your data lives
   // Open up your Google Spreadsheet
   // Grab the long string of text in the URL:
-  spreadsheetId: '',
+  1QmEBt_I_PLcvPij56z1G6bfyqel9xRHuXKhwoax455w/edit#gid=2045889131: '',
   // Enter your spreadsheet tab's name and
   // then the range you'd like to access:
-  spreadsheetRange: '',
+  Form Responses 1!B1:O99: '',
   // Enter the column header that holds the rows
   // that will be the titles of your Google docs:
-  title: '',
+ Nikki's (Responses): '',
 
   // GOOGLE DRIVE DETAILS, where do you want to put your docs?
   // Create a new folder in Drive or open an existing one
   // Grab the long string of text at the end of the URL:
-  driveFolderId: ''
+  1h0niL-sdfdCY16vV6vmvEHeKrG0_Oe7o: ''
 };
